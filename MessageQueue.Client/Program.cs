689740950eeb8fb46624c4b3de199e92ab563abd
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-24 00:25:12 +0100

    Implementing servicebus config

--- MessageQueue.Client/Program.cs
+++ MessageQueue.Client/Program.cs
@@ -36,6 +36,7 @@ namespace MessageQueue.Client
                     services.Databases(hostContext.Configuration.GetConnectionString("PrimaryConnection"));
                     services.Services();
                     services.Repositories();
+                    NServiceBusConfig(hostContext, services);
                 })
                 .ConfigureLogging((hostContext, configLogging) =>
                 {
@@ -59,6 +60,60 @@ namespace MessageQueue.Client
                 
                 await builder.RunConsoleAsync();
             }
+        }
+
+        private static Task NServiceBusConfig(HostBuilderContext hostContext, IServiceCollection services)
+        {
+            var serviceBusSettings = hostContext.Configuration.GetSection(nServiceBusSettings).Get<NServiceBusSettings>();
+
+            var endpointConfiguration = new EndpointConfiguration(serviceBusSettings.CrsHandlerEndpoint);
+
+            var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+            transport.ConnectionString(hostContext.Configuration.GetConnectionString(discConnection));
+            transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+
+            endpointConfiguration.AutoSubscribe();
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.UsePersistence<SqlPersistence>();
+            endpointConfiguration.UseContainer<ServicesBuilder>(c => c.ExistingServices(services));
+            endpointConfiguration.SendHeartbeatTo(serviceBusSettings.SendFailedMessagesTo);
+            endpointConfiguration.AuditProcessedMessagesTo(serviceBusSettings.AuditProcessedMessagesTo);
+            endpointConfiguration.SendFailedMessagesTo(serviceBusSettings.SendFailedMessagesTo);
+
+            var recoverability = endpointConfiguration.Recoverability();
+            recoverability.Immediate(
+                immediate =>
+                {
+                    immediate.NumberOfRetries(serviceBusSettings.NumberOfRetries);
+                });
+            recoverability.Delayed(
+                delayed =>
+                {
+                    delayed.NumberOfRetries(serviceBusSettings.NumberOfRetries);
+                    delayed.TimeIncrease(TimeSpan.FromSeconds(serviceBusSettings.RecoverabilityTimeIncreaseInSeconds));
+                });
+
+            //var metrics = endpointConfiguration.EnableMetrics();
+            //metrics.SendMetricDataToServiceControl(nServiceBusSettings.SendMetricDataToServiceControl,
+            //    TimeSpan.FromMilliseconds(nServiceBusSettings.SendMetricDataToServiceControlIntervalInMilliseconds));
+
+            var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+            persistence.SqlDialect<SqlDialect.MsSqlServer>();
+            persistence.ConnectionBuilder(
+                connectionBuilder: () =>
+                {
+                    return new SqlConnection(hostContext.Configuration.GetConnectionString(discConnection));
+                });
+
+            var subscriptions = persistence.SubscriptionSettings();
+            subscriptions.CacheFor(TimeSpan.FromMinutes(serviceBusSettings.SubscriptionCacheForInMinutes));
+
+            var endpointInstance = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            services.AddSingleton(endpointInstance);
+            services.AddSingleton<IMessageSession>(endpointInstance);
+
+            return Task.CompletedTask;
         }
     }
 }
