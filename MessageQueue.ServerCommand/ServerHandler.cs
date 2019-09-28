8469a6a2cf48636f35645cd00b9d5d6dc39e0f45
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-28 13:58:09 +0100

    Adding NServiceBus log

--- MessageQueue.ServerCommand/ServerHandler.cs
+++ MessageQueue.ServerCommand/ServerHandler.cs
@@ -1,14 +1,21 @@
 ﻿using MessageQueue.Domain.Entities;
 using NServiceBus;
+using NServiceBus.Logging;
 using System.Threading.Tasks;
 
 namespace MessageQueue.ServerCommand
 {
     public class ServerHandler : IHandleMessages<MessageCommandEntity>
     {
+        private readonly ILog nsbLog = LogManager.GetLogger<ServerHandler>();
+
         public Task Handle(MessageCommandEntity message, IMessageHandlerContext context)
         {
+            nsbLog.Info($"Message {message.Id} received");
+            
             // Implement logic and log
+
+
 
             return Task.CompletedTask;
         }
