159f7c860aa58ad77add948a42daeca7c7b0ee0e
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-28 01:59:35 +0100

    Adding command handler to server project

---
+++ MessageQueue.ServerCommand/ServerHandler.cs
@@ -0,0 +1,16 @@
+﻿using MessageQueue.Domain.Entities;
+using NServiceBus;
+using System.Threading.Tasks;
+
+namespace MessageQueue.ServerCommand
+{
+    public class ServerHandler : IHandleMessages<MessageCommandEntity>
+    {
+        public Task Handle(MessageCommandEntity message, IMessageHandlerContext context)
+        {
+            // Implement logic and log
+
+            return Task.CompletedTask;
+        }
+    }
+}
