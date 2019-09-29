3cfd44ae089b21bf496daa34342dc0492ea026a9
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-29 13:14:37 +0100

    Adjusting solution and adding project to publish an event

---
+++ MessageQueue.ClientEvent/ConsoleHost.cs
@@ -0,0 +1,52 @@
+using MessageQueue.Domain.Interfaces.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageQueue.ClientEvent
+{
+    public class ConsoleHost : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IEventService _eventService;
+        private readonly IEndpointInstance _endpointInstance;
+
+        public ConsoleHost(
+            ILogger<ConsoleHost> logger, 
+            IEventService eventService,
+            IEndpointInstance endpointInstance)
+        {
+            _logger = logger;
+            _eventService = eventService;
+            _endpointInstance = endpointInstance;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Core module started");
+
+            try
+            {
+                _eventService.PublishMessageAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception at ConsoleHost, Error: {e}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Core module stopped");
+
+            _endpointInstance?.Stop().ConfigureAwait(false);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MessageQueue.ClientEvent/LifetimeEventsServiceBase.cs
@@ -0,0 +1,88 @@
+﻿using MessageQueue.Domain.Interfaces.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageQueue.ClientEvent
+{
+    internal class LifetimeEventsServiceBase : ServiceBase, IHostLifetime
+    {
+        private readonly ILogger _logger;
+        private readonly IApplicationLifetime _appLifetime;
+        private readonly IEventService _eventService;
+        private readonly IEndpointInstance _endpointInstance;
+
+        public LifetimeEventsServiceBase(
+            ILogger<LifetimeEventsServiceBase> logger,
+            IApplicationLifetime appLifetime,
+            IEventService eventService,
+            IEndpointInstance endpointInstance)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+            _eventService = eventService;
+            _endpointInstance = endpointInstance;
+        }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            new Thread(Run).Start();
+
+            return Task.CompletedTask;
+        }
+
+        private void Run()
+        {
+            try
+            {
+                _logger.LogInformation("Windows service running");
+
+                Run(this); // This blocks until the service is stopped.
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception at Run method, Error: {ex}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Stop();
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        ///     Called by base.Run when the service is ready to start.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            _logger.LogInformation("Windows service started");
+
+            // Perform post-startup activities here
+            _eventService.PublishMessageAsync();
+
+            base.OnStart(args);
+        }
+
+        /// <summary>
+        ///     Called by base.Stop This may be called multiple times by service Stop
+        ///     StopApplication uses a CancellationTokenSource and prevents any recursion.
+        /// </summary>
+        protected override void OnStop()
+        {
+            _logger.LogInformation("Windows service stopped");
+
+            _endpointInstance?.Stop().ConfigureAwait(false);
+
+            _appLifetime.StopApplication();
+
+            base.OnStop();
+        }
+    }
+}
