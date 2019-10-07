2227ff3643dd67a7fb8a7c77b4378406e8c1379f
Aderbal Farias <aderbalfarias@hotmail.com>
2019-10-07 23:48:45 +0100

    Adding tracker class

---
+++ MessageQueue.Data/Mappings/TrackerMap.cs
@@ -0,0 +1,20 @@
+﻿using MessageQueue.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MessageQueue.Data.Mappings
+{
+    public class TrackerMap : IEntityTypeConfiguration<TrackerEntity>
+    {
+        public void Configure(EntityTypeBuilder<TrackerEntity> builder)
+        {
+            builder.ToTable("Tracker", "dbo");
+
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.ProjectName)
+                .HasMaxLength(500)
+                .IsRequired();
+        }
+    }
+}
