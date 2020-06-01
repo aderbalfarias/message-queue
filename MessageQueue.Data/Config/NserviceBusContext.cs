acd2077e86fd1dca4f661d41492473a00d5bbdfd
Aderbal Farias <aderbalfarias@hotmail.com>
2020-06-01 22:46:10 +0100

    Changing file name

--- MessageQueue.Data/Config/NserviceBusContext.cs
+++ MessageQueue.Data/Config/NserviceBusContext.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MessageQueue.Data.Config
 {
-    public class PrimaryContext : DbContext
+    public class NserviceBusContext : DbContext
     {
-        public PrimaryContext(DbContextOptions<PrimaryContext> options)
+        public NserviceBusContext(DbContextOptions<NserviceBusContext> options)
             : base(options)
         {
         }
